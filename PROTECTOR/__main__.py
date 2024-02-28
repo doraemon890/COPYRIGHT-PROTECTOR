67f2f03ba1a1f96e4828bd8bde131b9fb63c9b48
Rajnish <155803358+doraemon890@users.noreply.github.com>
2024-02-28 19:36:07 +0530

    first commit

---
+++ PROTECTOR/__main__.py
@@ -0,0 +1,21 @@
+import asyncio
+import importlib
+from pyrogram import idle
+from PROTECTOR import PROTECTOR
+from PROTECTOR.modules import ALL_MODULES
+
+LOGGER_ID = -1002024677280
+
+loop = asyncio.get_event_loop()
+
+async def JARVIS():
+    for all_module in ALL_MODULES:
+        importlib.import_module("PROTECTOR.modules." + all_module)
+    print("Bot Started Successfully")
+    await idle()
+    print("MAI HU PIRO CODER BOLO NHI AAYA ERROR")
+    await PROTECTOR.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [JARVIS](https://t.me/jarvis2O)**")
+
+if __name__ == "__main__":
+    loop.run_until_complete(JARVIS())
+    
---
+++ PROTECTOR/modules/main.py
@@ -0,0 +1,169 @@
+from pyrogram import Client, filters
+import os
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram import filters
+from pyrogram.types import Message
+import time
+import psutil
+import platform
+import logging
+from config import OWNER_ID, BOT_USERNAME
+from config import *
+from PROTECTOR import PROTECTOR as app
+
+import pyrogram
+from pyrogram.errors import FloodWait
+
+
+# ----------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------
+
+
+start_txt = """<b> 🤖 ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ 🛡️ </b>
+
+ʜᴇʏ ᴛʜɪs ɪs ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ ʀᴏʙᴏᴛ🤖!\n ᴡᴇ ᴇɴsᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ sᴇᴄᴜʀɪᴛʏ💻 !\n ᴛʜɪs ʙᴏᴛ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ʟᴏɴɢ ᴛᴇxᴛ ᴇᴅɪᴛᴇᴅ ᴍsɢs , ᴀɴᴅ ᴄᴏᴘʏʀɪɢʜᴛ ᴍᴀᴛᴇʀɪᴀʟ...!\nᴊᴜsᴛ ᴀᴅᴅ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ᴀᴅᴍɪɴ !!\nғᴇᴇʟ ғʀᴇᴇ ғʀᴏᴍ ᴀɴʏ ᴛʏᴘᴇ ᴏғ ᴄᴏᴘʏʀɪɢʜᴛ... ! 🛡! 🤝🔐 """
+
+@app.on_message(filters.command("start"))
+async def start(_, msg):
+    buttons = [
+        [ 
+          InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        [
+          InlineKeyboardButton("• ʜᴀɴᴅʟᴇʀ •", callback_data="vip_back")
+        ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://telegra.ph/file/3c98dbb4e941e05912697.jpg",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
+
+
+gd_buttons = [              
+        [
+            InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=OWNER_ID),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/CHATTING_2024"),    
+        ]
+        ]
+
+
+# ------------------------------------------------------------------------------- #
+
+
+@app.on_callback_query(filters.regex("vip_back"))
+async def vip_back(_, query: CallbackQuery):
+    await query.message.edit_caption(start_txt,
+                                    reply_markup=InlineKeyboardMarkup(gd_buttons),)
+        
+
+# -------------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------------
+
+
+start_time = time.time()
+
+def time_formatter(milliseconds: float) -> str:
+    seconds, milliseconds = divmod(milliseconds, 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
+
+def size_formatter(bytes: int) -> str:
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if bytes < 1024.0:
+            break
+        bytes /= 1024.0
+    return f"{bytes:.2f} {unit}"
+
+
+
+@app.on_message(filters.command("ping"))
+async def activevc(_, message: Message):
+    uptime = time_formatter((time.time() - start_time) * 1000)
+    cpu = psutil.cpu_percent()
+    storage = psutil.disk_usage('/')
+
+    python_version = platform.python_version()
+
+    reply_text = (
+        f"➪ᴜᴘᴛɪᴍᴇ: {uptime}\n"
+        f"➪ᴄᴘᴜ: {cpu}%\n"
+        f"➪ꜱᴛᴏʀᴀɢᴇ: {size_formatter(storage.total)} [ᴛᴏᴛᴀʟ]\n"
+        f"➪{size_formatter(storage.used)} [ᴜsᴇᴅ]\n"
+        f"➪{size_formatter(storage.free)} [ғʀᴇᴇ]\n"
+        f"➪ᴊᴀʀᴠɪs ᴠᴇʀsɪᴏɴ: {python_version},"
+    )
+
+    await message.reply(reply_text, quote=True)
+
+
+    
+# -------------------------------------------------------------------------------------
+
+
+
+FORBIDDEN_KEYWORDS = ["porn", "xxx", "NCERT","ncert", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt", "JEE", "ALLEN" "NEET", "jee", "neet", "ans"]
+
+@app.on_message()
+async def handle_message(client, message):
+    if any(keyword in message.text for keyword in FORBIDDEN_KEYWORDS):
+        logging.info(f"Deleting message with ID {message.id}")
+        await message.delete()
+      #  user_mention = from_user.mention
+        await message.reply_text(f"@{message.from_user.username} 𝖣𝗈𝗇'𝗍 𝗌𝖾𝗇𝖽 𝗇𝖾𝗑𝗍 𝗍𝗂𝗆𝖾!")
+    elif any(keyword in message.caption for keyword in FORBIDDEN_KEYWORDS):
+        logging.info(f"Deleting message with ID {message.id}")
+        await message.delete()
+       # user_mention = from_user.mention
+        await message.reply_text(f"@{message.from_user.username} 𝖣𝗈𝗇'𝗍 𝗌𝖾𝗇𝖽 𝗇𝖾𝗑𝗍 𝗍𝗂𝗆𝖾!")
+        
+        
+# -------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
+@app.on_edited_message(filters.group & ~filters.me)
+async def delete_edited_messages(client, edited_message):
+    await edited_message.delete()
+
+
+
+# ----------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------
+def delete_long_messages(_, m):
+    return len(m.text.split()) > 10
+
+@app.on_message(filters.group & filters.private & delete_long_messages)
+async def delete_and_reply(_, msg):
+    await msg.delete()
+    user_mention = msg.from_user.mention
+    await app.send_message(msg.chat.id, f"Hey {user_mention}, please keep your messages short!")
+    
+
+# -----------------------------------------------------------------------------------
+
+
+    
+@app.on_message(filters.animation | filters.audio | filters.document | filters.photo | filters.sticker | filters.video)
+async def keep_reaction_message(client, message: Message):
+    pass 
+# -------------------------------
+
+async def delete_pdf_files(client, message):
+    if message.document and message.document.mime_type == "application/pdf":
+        warning_message = f"@{message.from_user.username} ʙᴇᴛᴀ ᴊɪ ᴘᴅғ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ,\n  ᴄᴏᴘʏʀɪɢʜᴛ ʟᴀɢʏᴇɢᴀ🤣🤣"
+        await message.reply_text(warning_message)
+        await message.delete()
+    else:  
+        pass
+
+@app.on_message(filters.group & filters.document)
+async def message_handler(client, message):
+    await delete_pdf_files(client, message)
+
+# ----------------------------------------
