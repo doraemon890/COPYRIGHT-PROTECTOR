56b7440e71e176f79e26ccb8dbdb7b2c7af5d862
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-29 10:40:46 +0530

    Update main.py

--- PROTECTOR/modules/main.py
+++ PROTECTOR/modules/main.py
@@ -1,69 +1,52 @@
-from pyrogram import Client, filters
-import os
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram import filters
-from pyrogram.types import Message
-import time
-import psutil
-import platform
 import logging
-from config import OWNER_ID, BOT_USERNAME
-from config import *
+import os
+import platform
+import psutil
+import time
+
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
+from config import BOT_USERNAME, OWNER_ID
 from PROTECTOR import PROTECTOR as app
+from config import *
 
-import pyrogram
-from pyrogram.errors import FloodWait
-
-# ----------------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------------
-
-# -------------------------------------------------------------------------------------
-
-start_txt = """<b> 🤖 ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ 🛡️ </b>
+# Constants
+FORBIDDEN_KEYWORDS = ["porn", "xxx", "NCERT", "ncert", "ans", "Pre-Medical", "Pollen germination and pollen tube growth are regulated by chemical components of pollen interacting with those of the pistil", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt", "JEE", "ALLEN", "NEET", "jee", "neet", "ans"]
+START_TEXT = """<b> 🤖 ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ 🛡️ </b>
 
 ʜᴇʏ ᴛʜɪs ɪs ᴄᴏᴘʏʀɪɢʜᴛ ᴘʀᴏᴛᴇᴄᴛᴏʀ ʀᴏʙᴏᴛ🤖!\n ᴡᴇ ᴇɴsᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ sᴇᴄᴜʀɪᴛʏ💻 !\n ᴛʜɪs ʙᴏᴛ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ʟᴏɴɢ ᴛᴇxᴛ ᴇᴅɪᴛᴇᴅ ᴍsɢs , ᴀɴᴅ ᴄᴏᴘʏʀɪɢʜᴛ ᴍᴀᴛᴇʀɪᴀʟ...!\nᴊᴜsᴛ ᴀᴅᴅ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ᴀᴅᴍɪɴ !!\nғᴇᴇʟ ғʀᴇᴇ ғʀᴏᴍ ᴀɴʏ ᴛʏᴘᴇ ᴏғ ᴄᴏᴘʏʀɪɢʜᴛ... ! 🛡! 🤝🔐 """
 
+
+
+# ------------------------------------------------------------------------------------------
+# Command Handlers
+
 @app.on_message(filters.command("rajnishayushilovemaineteranaamdilrkhdiya"))
-async def start(_, msg):
+async def start_command_handler(_, msg):
     buttons = [
-        [ 
-          InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
-        ],
-        [
-          InlineKeyboardButton("• ʜᴀɴᴅʟᴇʀ •", callback_data="vip_back")
-        ]]
-    
+        [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [InlineKeyboardButton("• ʜᴀɴᴅʟᴇʀ •", callback_data="vip_back")]
+    ]
     reply_markup = InlineKeyboardMarkup(buttons)
-    
     await msg.reply_photo(
         photo="https://telegra.ph/file/3c98dbb4e941e05912697.jpg",
-        caption=start_txt,
+        caption=START_TEXT,
         reply_markup=reply_markup
     )
 
+# Callback Query Handler
 
-gd_buttons = [              
-        [
-            InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=OWNER_ID),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/CHATTING_2024"),    
-        ]
-        ]
-
-
-# ------------------------------------------------------------------------------- #
-
+gd_buttons = [
+    [InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=OWNER_ID),
+     InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/CHATTING_2024")],
+]
 
 @app.on_callback_query(filters.regex("vip_back"))
-async def vip_back(_, query: CallbackQuery):
-    await query.message.edit_caption(start_txt,
-                                    reply_markup=InlineKeyboardMarkup(gd_buttons),)
-        
+async def vip_back_callback_handler(_, query: CallbackQuery):
+    await query.message.edit_caption(START_TEXT, reply_markup=InlineKeyboardMarkup(gd_buttons))
 
-# -------------------------------------------------------------------------------------
-
-
-# -------------------------------------------------------------------------------------
-
+# ------------------------------------------------------------------------------------------
+# Bot Functionality
 
 start_time = time.time()
 
@@ -79,8 +62,6 @@ def size_formatter(bytes: int) -> str:
             break
         bytes /= 1024.0
     return f"{bytes:.2f} {unit}"
-
-
 
 @app.on_message(filters.command("jarvisxd85"))
 async def activevc(_, message: Message):
@@ -101,48 +82,35 @@ async def activevc(_, message: Message):
 
     await message.reply(reply_text, quote=True)
 
-
-    
-# -------------------------------------------------------------------------------------
-
-
-
-FORBIDDEN_KEYWORDS = ["porn", "xxx", "NCERT","ncert","ans","Pre-Medical", " Pollen germination and pollen tube growth are regulated by chemical components of pollen interacting with those of the pistil" , "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt", "JEE", "ALLEN" "NEET", "jee", "neet", "ans"]
+# Handle Forbidden Keywords
 
 @app.on_message()
 async def handle_message(client, message):
     if any(keyword in message.text for keyword in FORBIDDEN_KEYWORDS):
         logging.info(f"Deleting message with ID {message.id}")
         await message.delete()
-      #  user_mention = from_user.mention
         await message.reply_text(f"@{message.from_user.username} 𝖣𝗈𝗇'𝗍 𝗌𝖾𝗇𝖽 𝗇𝖾𝗑𝗍 𝗍𝗂𝗆𝖾!")
     elif any(keyword in message.caption for keyword in FORBIDDEN_KEYWORDS):
         logging.info(f"Deleting message with ID {message.id}")
         await message.delete()
-       # user_mention = from_user.mention
         await message.reply_text(f"@{message.from_user.username} 𝖣𝗈𝗇'𝗍 𝗌𝖾𝗇𝖽 𝗇𝖾𝗑𝗍 𝗍𝗂𝗆𝖾!")
 
-        
-        
-# -------------------------------------------------------------------------------------
 # Delete long edited messages but keep short messages and emoji reactions
+
 async def delete_long_edited_messages(client, edited_message: Message):
-    # Check if the edited message contains text
     if edited_message.text:
-        # Check if the message is considered long (more than 25 words)
         if len(edited_message.text.split()) > 20:
             await edited_message.delete()
     else:
-        # If the edited message does not contain text, check for emoji reactions
         if edited_message.sticker or edited_message.animation or edited_message.emoji:
-            return  # Leave emoji reactions untouched
+            return
 
 @app.on_edited_message(filters.group & ~filters.me)
 async def handle_edited_messages(_, edited_message: Message):
     await delete_long_edited_messages(_, edited_message)
 
-# ----------------------------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------------------------
+# Delete long messages in groups and reply with a warning
+
 def delete_long_messages(_, m):
     return len(m.text.split()) > 20
 
@@ -151,8 +119,4 @@ async def delete_and_reply(_, msg):
     await msg.delete()
     user_mention = msg.from_user.mention
     await app.send_message(msg.chat.id, f"Hey {user_mention}, please keep your messages short!")
-    
 
-
-
-# ----------------------------------------
