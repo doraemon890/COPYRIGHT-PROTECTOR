46eb8d04983ff8694719ed76e75b1c067b638798
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-10 09:40:16 +0530

    Update usersdb.py

--- PROTECTOR/helper/usersdb.py
+++ PROTECTOR/helper/usersdb.py
@@ -2,38 +2,25 @@ from config import MONGO_URL
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 mongo = MongoCli(MONGO_URL)
-db = mongo.users
-
-db = db.users
-
-
-
+db_users = mongo.usersdb
 
 async def get_users():
-  user_list = []
-  async for user in db.users.find({"user": {"$gt": 0}}):
-    user_list.append(user['user'])
-  return user_list
-
+    user_list = []
+    async for user in db_users.find({"user": {"$gt": 0}}):
+        user_list.append(user['user'])
+    print(f"Debug: get_users() -> {user_list}")
+    return user_list
 
 async def get_user(user):
-  users = await get_users()
-  if user in users:
-    return True
-  else:
-    return False
+    users = await get_users()
+    return user in users
 
 async def add_user(user):
-  users = await get_users()
-  if user in users:
-    return
-  else:
-    await db.users.insert_one({"user": user})
-
+    users = await get_users()
+    if user not in users:
+        await db_users.insert_one({"user": user})
 
 async def del_user(user):
-  users = await get_users()
-  if not user in users:
-    return
-  else:
-    await db.users.delete_one({"user": user})
+    users = await get_users()
+    if user in users:
+        await db_users.delete_one({"user": user})
