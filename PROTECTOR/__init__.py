51b7cb31088bef29d42e5b8d66da96a28bf08b21
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-25 10:54:39 -0700

    Update __init__.py

--- PROTECTOR/__init__.py
+++ PROTECTOR/__init__.py
@@ -7,14 +7,20 @@ from dotenv import load_dotenv
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, BOT_USERNAME, OWNER_ID
 
+
+
+
 loop = asyncio.get_event_loop()
 load_dotenv()
 boot = time.time()
+
 
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
     level=logging.INFO,
 )
+
+
 
 PROTECTOR = Client(
     ":PROTECTOR:",
@@ -23,21 +29,18 @@ PROTECTOR = Client(
     bot_token=BOT_TOKEN,
 )
 
+
+
 async def PROTECTOR_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
-    try:
-        await PROTECTOR.start()
-        getme = await PROTECTOR.get_me()
-        BOT_ID = getme.id
-        BOT_USERNAME = getme.username
-        if getme.last_name:
-            BOT_NAME = getme.first_name + " " + getme.last_name
-        else:
-            BOT_NAME = getme.first_name
-        logging.info(f"Bot started successfully as {BOT_NAME} (@{BOT_USERNAME})")
-    except Exception as e:
-        logging.error(f"Failed to start the bot: {e}")
-    finally:
-        await PROTECTOR.stop()
+    await PROTECTOR.start()
+    getme = await PROTECTOR.get_me()
+    BOT_ID = getme.id
+    BOT_USERNAME = getme.username
+    if getme.last_name:
+        BOT_NAME = getme.first_name + " " + getme.last_name
+    else:
+        BOT_NAME = getme.first_name
+
 
 loop.run_until_complete(PROTECTOR_bot())
