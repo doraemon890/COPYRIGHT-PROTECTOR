76b1b81e1d8fdb6fa8dabd1ffcf275a82894b490
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-25 11:04:38 -0700

    Update __main__.py

--- PROTECTOR/__main__.py
+++ PROTECTOR/__main__.py
@@ -14,7 +14,7 @@ async def JARVIS():
     print("Bot Started Successfully")
     await idle()
     print("MAI HU PIRO CODER BOLO NHI AAYA ERROR")
-    await PROTECTOR.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [JARVIS](https://t.me/JARVIS_V2)**")
+    await PROTECTOR.send_message(LOGGER_ID, "**ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ ʏᴏᴜʀ ʙᴏᴛ ᴅᴇᴘʟᴏʏᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ✅ \n ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ  [JARVIS](https://t.me/JARVIS_V2)**")
 
 if __name__ == "__main__":
     loop.run_until_complete(JARVIS())
