7ac9ccde0e8055d1a5343ace09c56984fb76a50b
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-08-01 09:06:07 +0530

    Update mongo.py

--- PROTECTOR/helper/mongo.py
+++ PROTECTOR/helper/mongo.py
@@ -5,7 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 # Mongo Database
 try:
-    mongodb = AsyncIOMotorClient(Config.MONGO_URL).ULTRON
+    mongodb = AsyncIOMotorClient(config.MONGO_URL).ULTRON
 except:
     print("Please Change Your MongoDB")
     sys.exit()
